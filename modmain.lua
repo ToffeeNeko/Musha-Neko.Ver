102fd0186f97a03096107f54d5b52f734fd5bdeb
ToffeeNeko <toffeeneko@outlook.com>
2022-07-02 15:44:07 -0500

    Add notes for imported scripts

--- modmain.lua
+++ modmain.lua
@@ -10,16 +10,16 @@ local modlanguage = GetModConfigData("modlanguage")
 AddReplicableComponent("mana")
 
 -- Run scripts
-modimport("scripts/lib/tuning.lua")
-modimport("scripts/lib/prefabs.lua")
-modimport("scripts/lib/assets.lua")
-modimport("scripts/lib/skins.lua")
-modimport("scripts/lib/recipes.lua")
-modimport("scripts/lib/actions.lua")
-modimport("scripts/lib/hotkeys.lua")
-modimport("scripts/lib/stategraphs.lua")
-modimport("scripts/lib/player_classified.lua")
-modimport("scripts/lib/statusdisplays.lua")
+modimport("scripts/lib/tuning.lua") -- Settings, values and parameters
+modimport("scripts/lib/prefabs.lua") -- Prefab list
+modimport("scripts/lib/assets.lua") -- Asset list
+modimport("scripts/lib/skins.lua") -- Show skins on character creation
+modimport("scripts/lib/recipes.lua") -- Recipe list
+modimport("scripts/lib/actions.lua") -- Redefine certain character actions (attack, eat, etc.)
+modimport("scripts/lib/hotkeys.lua") -- Add key handlers
+modimport("scripts/lib/stategraphs.lua") -- Add action modules and anims (smite,etc.)
+modimport("scripts/lib/player_classified.lua") -- Redefine prefabs/player_classified for replicable components like mana/stamina/fatigue
+modimport("scripts/lib/statusdisplays.lua") -- Settings for mana/stamina/fatigue badges display
 
 -- Custom strings (i18n)
 if modlanguage == "english" then
